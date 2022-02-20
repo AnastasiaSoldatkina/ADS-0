b48b222a1081a727c55906597422f2d5c685daf5
AnastasiaSoldatkina <94635746+AnastasiaSoldatkina@users.noreply.github.com>
2022-02-20 19:33:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,9 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    int i=a;
-    while(i>0){
-        if (a%i==0 && b%i==0){
+    int i = a;
+    while (i > 0) {
+        if (a%i == 0 && b%i == 0) {
             break;
         }
         i--;
