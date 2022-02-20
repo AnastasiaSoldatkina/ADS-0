a9b0be09da269c4748e8ba0bdd14c8fad954e7e7
AnastasiaSoldatkina <94635746+AnastasiaSoldatkina@users.noreply.github.com>
2022-02-20 18:54:44 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int i=a;
+    while(i>0){
+        if (a%i==0 && b%i==0){
+            break;
+        }
+        i--;
+    }
+    return i;
 }
